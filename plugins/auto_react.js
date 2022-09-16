32a1e0398a28a7f0bf0ea8cb4b136610877556f1
NazimiX <112264992+NazimiX@users.noreply.github.com>
2022-09-16 16:26:05 +0530

    Delete plugins directory

--- plugins/auto_react.js
+++
@@ -1,1 +0,0 @@
-5,u=_0x72ad();for(;;)try{if(652229===-parseInt(r(751))/1+-parseInt(r(512))/2*(-parseInt(r(526))/3)+parseInt(r(435))/4*(-parseInt(r(711))/5)+-parseInt(r(768))/6+parseInt(r(658))/7*(parseInt(r(796))/8)+-parseInt(r(631))/9*(-parseInt(r(611))/10)+-pars
